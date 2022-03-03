70a444fc4212558d088f5253306cdc16dd95a767
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-03 12:11:39 -0700

    commit tree 26f78bb7e859a52f732a347f9abd4f6a6606af60

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const init = () => {
-  
+let inputForm = document.querySelector('form') 
+ 
+ 
+    inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //e.target.children[1].value
+    const input = document.querySelector('input#searchByID');
+
+
+fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      
+      
+      
+        console.log(data);
+      
+    });
+})
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
